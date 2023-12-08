4ff620ac02f5aa5842ba1f3674af7f1c8008d1cd
Yegeta <149103185+Yegetaa@users.noreply.github.com>
2023-12-08 13:36:24 -0500

    part 1 completed

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const adventurer = {
     name: "Robin",
     health: 10,
-    inventory: ["sword", "potion", "artifact"]
-    }
-    
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+        name: "Leo",
+        type: "Cat"
+    }, 
+    subCompanion: {
+        name: "Frank",
+        type: "Flea",
+        inventory: ["small hat", "sunglasses"]
+    },
+    roll (mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`)
+        }
+}
+
 //Loop that logs each item in Robin's inventory 
 for (let i = 0; i < adventurer.inventory.length; i++) {
     console.log(adventurer.inventory[i]);    
 }
+
+console.log(adventurer.roll(6));
