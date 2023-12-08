be6b0d2abb27d5a433bacd7e5d744694d635729f
Yegeta <149103185+Yegetaa@users.noreply.github.com>
2023-12-08 13:22:15 -0500

    1 adventurer added

---
+++ script.js
@@ -0,0 +1,10 @@
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"]
+    }
+    
+//Loop that logs each item in Robin's inventory 
+for (let i = 0; i < adventurer.inventory.length; i++) {
+    console.log(adventurer.inventory[i]);    
+}
